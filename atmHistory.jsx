ac1de16485a1dfb00b8a2d449e8479633f22d29d
sullybjimenez <sullybjimenez@gmail.com>
2023-02-26 19:49:01 -0500

    uploaded files

---
+++ atmHistory.jsx
@@ -0,0 +1,10 @@
+const ATMHistory = () => {
+    
+    return (
+        <label className="label huge">
+            <h3> Account History </h3>
+        </label>
+    );
+};
+
+ReactDOM.render(<ATMHistory/>, document.getElementById("history"));
